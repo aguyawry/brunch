632e2d91ba414de632b30066376ef88154f0c331
Scott Elson <aguyawry@gmail.com>
2018-04-20 22:05:44 -0400

    blah

--- app/initialize.js
+++ app/initialize.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+	console.log('Top Initialized app');
 
+	var express = require( 'express' );
+	var nunjucks = require( 'nunjucks' );
+	var app = express();
 
-	require('nunjucks');
-	nunjucks.configure('views', { autoescape: true });
-	nunjucks.render('index.html', { foo: 'bar' });
+	var PATH_TO_TEMPLATES = '/views' ;
+	nunjucks.configure( PATH_TO_TEMPLATES, {
+    	autoescape: true,
+    	express: app
+	} ) ;
+
+	
+	var index = nunjucks.render('index.html');
 	
   	console.log('Initialized app');
 });
